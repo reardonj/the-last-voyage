2baf037b6b2ec2d168993eccd9ee548e1d9bdcdc
Justin Reardon <justin.reardon@gmail.com>
2020-11-28 13:58:12 -0500

    ignore the original mp3 files for now

--- webpack.config.js
+++ webpack.config.js
@@ -5,61 +5,61 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-	entry: {
-		app: './src/ts/Game.ts'
-	},
-	module: {
-		rules: [{
-			test: /\.tsx?$/,
-			use: 'ts-loader',
-			exclude: /node_modules/
-		}]
-	},
-	resolve: {
-		extensions: [ '.tsx', '.ts', '.js' ]
-	},
-	externals: {
-		phaser: 'Phaser'
-	},
-	output: {
-		filename: 'app.js',
-		path: path.resolve(__dirname, 'public')
-	},
-	devtool: "source-map",
-	plugins: [
-		new CleanWebpackPlugin(),
-		new CopyWebpackPlugin({
-			patterns: [
-				{
-					from: './node_modules/phaser/dist/phaser.min.js',
-					to: 'lib'
-				},
-				{
-					from: './src/*.html',
-					to: path.resolve(__dirname, 'public'),
-					flatten: true
-				},
-				{
-					from: './src/*.css',
-					to: path.resolve(__dirname, 'public'),
-					flatten: true
-				},
-				{
-					from: './src/*.ico',
-					to: path.resolve(__dirname, 'public'),
-					flatten: true
-				},
-				{
-					from: './src/assets',
-					to: 'assets',
-					globOptions: {
-						ignore: [ '*.md' ]
-					}
-				}
-			]
-		})
-	],
-	devServer: {
-		open: true
-	}
+  entry: {
+    app: './src/ts/Game.ts'
+  },
+  module: {
+    rules: [{
+      test: /\.tsx?$/,
+      use: 'ts-loader',
+      exclude: /node_modules/
+    }]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  externals: {
+    phaser: 'Phaser'
+  },
+  output: {
+    filename: 'app.js',
+    path: path.resolve(__dirname, 'public')
+  },
+  devtool: "source-map",
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: './node_modules/phaser/dist/phaser.min.js',
+          to: 'lib'
+        },
+        {
+          from: './src/*.html',
+          to: path.resolve(__dirname, 'public'),
+          flatten: true
+        },
+        {
+          from: './src/*.css',
+          to: path.resolve(__dirname, 'public'),
+          flatten: true
+        },
+        {
+          from: './src/*.ico',
+          to: path.resolve(__dirname, 'public'),
+          flatten: true
+        },
+        {
+          from: './src/assets',
+          to: 'assets',
+          globOptions: {
+            ignore: ['**.md', '**.mp3']
+          }
+        }
+      ]
+    })
+  ],
+  devServer: {
+    open: true
+  }
 };
